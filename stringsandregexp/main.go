62147f47a6ab1f58dabac04f4858d61b9bac8759
swvalerian <valera.panafidin@teamgetter.com>
2025-04-11 13:28:48 +0900

    feat: regexp complete

--- stringsandregexp/main.go
+++ stringsandregexp/main.go
@@ -8,10 +8,34 @@ import (
 
 func main() {
 	description := "A boat for one person"
+
+	// матчим регулярные выражения, иными словами - ищем совпадения
 	match, err := regexp.MatchString("[A-z]oat", description)
 	if err == nil {
 		fmt.Println("Match:", match)
 	} else {
 		fmt.Println("Error:", err)
 	}
+
+	// вариант другой
+	pattern, compileErr := regexp.Compile("[A-z]oat")
+
+	question := "Is that a goat?"
+	preference := "I like oats" // !
+	if compileErr == nil {
+		fmt.Println("Description:",
+			pattern.MatchString(description))
+		fmt.Println("Question:",
+			pattern.MatchString(question))
+		fmt.Println("Preference:",
+			pattern.MatchString(preference))
+	} else {
+		fmt.Println("Error:", compileErr)
+	}
+
+	// совпадение по статичной части и изменяющейся
+	pattern2 := regexp.MustCompile("A [A-z]* for [A-z]* person")
+
+	str := pattern2.FindString(description)
+	fmt.Println("Match:", str)
 }
